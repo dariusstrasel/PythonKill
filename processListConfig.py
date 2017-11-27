5574a6d302b334795eae26a1de142721109cfc52
EMA\dstrasel <dstrasel@emoneyadvisor.com>
2017-11-27 11:10:27 -0500

    Add ternary operator to grab environmentvariable processList, if it exists.

--- processListConfig.py
+++ processListConfig.py
@@ -1,1 +1,5 @@
-processList = ["chromedriver.exe"]
+import os
+
+defaultList = ["chromedriver.exe"]
+
+processList = defaultList if os.environ.get('processList') == None else os.environ.get('processList')
