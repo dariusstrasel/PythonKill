50d011583369e057a808511dfba4f0f99dc78e1a
EMA\dstrasel <dstrasel@emoneyadvisor.com>
2017-11-27 11:30:36 -0500

    Change default seconds to 0.

--- processListConfig.py
+++ processListConfig.py
@@ -1,5 +1,5 @@
 import os
 
 DEFAULT_LIST = ["chromedriver.exe"]
-SECONDS_TO_LOOP_FOR = 10
+SECONDS_TO_LOOP_FOR = 0
 PROCESS_LIST = DEFAULT_LIST if os.environ.get('processList') == None else os.environ.get('processList')
